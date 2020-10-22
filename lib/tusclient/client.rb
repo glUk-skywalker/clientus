126758f0e8712cf6bd54f5de577d4cb9f15a48d2
Oleg Ivanov <gluk.main+github@gmail.com>
2020-10-22 20:06:50 +0300

    uploading roughly implemented

--- lib/tusclient/client.rb
+++ lib/tusclient/client.rb
@@ -3,6 +3,8 @@
 module TusClient
   # Client that transfers files. The main class.
   class Client
+    CHUNK_SIZE = 100 * 1024 * 1024 # 100MB
+
     def initialize(url, http_params: {})
       @server_uri = URI.parse url
       @http = Net::HTTP.start(@server_uri.host, @server_uri.port, http_params)
@@ -13,6 +15,21 @@ module TusClient
 
     def upload(file_path, additional_headers: {})
       @server.supports_create? || raise(Error, 'the server does not support file creation')
+
+      file = read_file(file_path)
+
+      location = create_file(file, additional_headers: additional_headers)
+
+      start_offset = 0
+      loop do
+        end_offset = start_offset + CHUNK_SIZE
+        chunk_data = file.data[start_offset..end_offset]
+
+        upload_chunk(location, start_offset, chunk_data, additional_headers: additional_headers)
+        break if end_offset >= file.size
+
+        start_offset = end_offset + 1
+      end
     end
 
     private
@@ -31,19 +48,24 @@ module TusClient
       request['Content-Length'] = 0
       request['Upload-Length'] = file.size
       request['Tus-Resumable'] = @server.tus_version
-
-      request['Cookie'] = additional_headers['Cookie']&.map { |k, v| "#{k}=#{v}"}.join(';')
-
       request['Upload-Metadata'] = "filename #{file.encoded_name}"
-      additional_headers['Upload-Metadata']&.each do |k, v|
-        request['Upload-Metadata'] = [request['Upload-Metadata'], "#{k} #{v}"].join(',')
-      end
+      TusClient.adjust_headers(request, additional_headers)
 
       res = @http.request(request)
-      puts res['Location']
-      return if res.is_a?(Net::HTTPCreated)
+      return res['Location'] if res.is_a?(Net::HTTPCreated)
 
-      raise(Error, "bad resonse type: #{res.class}; body: #{res.body}")
+      raise(Error, "failed to create remote file: bad resonse type: #{res.class}; body: #{res.body}")
+    end
+
+    def upload_chunk(uri, offset, chunk, additional_headers: {})
+      request = Net::HTTP::Patch.new(uri)
+      request['Content-Type'] = 'application/offset+octet-stream'
+      request['Upload-Offset'] = offset
+      request['Tus-Resumable'] = @server.tus_version
+      request.body = chunk
+      TusClient.adjust_headers(request, additional_headers)
+
+      @http.request(request)
     end
   end
 end
