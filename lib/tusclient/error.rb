2476c8a3948ce1763f4a7515dec8d40785c7bceb
Oleg Ivanov <gluk.main+github@gmail.com>
2020-10-22 16:24:32 +0300

    initial: the gem skeleton

---
+++ lib/tusclient/error.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tusclient
+  class Error < StandardError; end
+end
---
+++ lib/tusclient/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tusclient
+  VERSION = '0.0.0.dev'
+end
