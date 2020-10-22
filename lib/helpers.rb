ada1a7a22629e636f38af87a1597799c2b33d7ac
Oleg Ivanov <gluk.main+github@gmail.com>
2020-10-22 19:59:07 +0300

    TusClient.adjust_header: a handy helper for headers adjustment

---
+++ lib/helpers.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# The main module
+module TusClient
+  def self.adjust_headers(request, additional_headers)
+    request['Cookie'] = additional_headers['Cookie']&.map { |k, v| "#{k}=#{v}"}.join(';')
+    additional_headers['Upload-Metadata']&.each do |k, v|
+      request['Upload-Metadata'] = [request['Upload-Metadata'], "#{k} #{v}"].join(',')
+    end
+  end
+end
