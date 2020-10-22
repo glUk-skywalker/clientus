61709b3bb3343f2f4eb76238bb54570cbb07f817
Oleg Ivanov <gluk.main+github@gmail.com>
2020-10-22 18:52:12 +0300

    UploadFile class + require

---
+++ lib/tusclient/upload_file.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Tusclient
+  # Holds file information
+  class UploadFile
+    attr_reader :name, :size, :data
+
+    def initialize(name, size, data)
+      @name = name
+      @data = data
+      @size = size
+    end
+
+    def encoded_name
+      # TODO: investigate why we need to exclude CR/LF in the end of encoded string
+      Base64.encode64(@name)[0..-2]
+    end
+  end
+end
