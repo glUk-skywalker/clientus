d0239c0a099dbd0054bbcbfa07e43503831527df
Oleg Ivanov <gluk.main+github@gmail.com>
2020-10-22 16:42:11 +0300

    basic initialization

--- lib/tusclient.rb
+++ lib/tusclient.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
+require 'net/http'
+
 require 'tusclient/version'
+require 'tusclient/client'
--- lib/tusclient/client.rb
+++ lib/tusclient/client.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 module Tusclient
-  class Client; end
+  # Client that transfers files. The main class.
+  class Client
+    def initialize(url, http_params = {})
+      @server_uri = URI.parse url
+      @http = Net::HTTP.start(@server_uri.host, @server_uri.port, http_params)
+    end
+  end
 end
