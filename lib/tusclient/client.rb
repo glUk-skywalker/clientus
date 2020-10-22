e1b910bd1f27553cc111eba982c7a80833c27a74
Oleg Ivanov <gluk.main+github@gmail.com>
2020-10-22 17:30:47 +0300

    Client class: http_params is now named argument

--- lib/tusclient/client.rb
+++ lib/tusclient/client.rb
@@ -3,7 +3,7 @@
 module Tusclient
   # Client that transfers files. The main class.
   class Client
-    def initialize(url, http_params = {})
+    def initialize(url, http_params: {})
       @server_uri = URI.parse url
       @http = Net::HTTP.start(@server_uri.host, @server_uri.port, http_params)
     end
